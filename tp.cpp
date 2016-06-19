1671be54b5e62c47f82768a80cb96a952519ec3c
Mariafilipa <filipaserra2004@gmail.com>
2016-06-19 21:59:34 +0000

    Falta pausa, reflexões e pontuação

--- tp.cpp
+++ tp.cpp
@@ -3,17 +3,16 @@
 #include <string.h>
 #include <math.h>
 #include <time.h>
+
 // INCLUDES MARIA
-/*
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
-*/
 
 //INCLUDES JORGE
-#include <OpenGL/gl.h>
+/*#include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
-#include <GLUT/glut.h>
+#include <GLUT/glut.h>*/
 
 #include "RgbImage.h"
 
@@ -42,19 +41,28 @@ GLfloat   cube1_position_z;
 GLint     hit = 0;
 GLint 		exit_game=0;
 GLint 		pause_game=0;
-GLfloat   jump_coordinate;
+GLfloat   jump_coordinate=0.5;
 GLfloat   jump_speed = 0.20;
 GLfloat  	inc   = 0.5;
 GLint     jump = 0;
-GLfloat   aux_ball_speed;
-GLfloat   aux_ball_speed_x;
-
+GLint 		ball_type=0;
+GLint 		ball_aux=0;
 
 // Observador
+GLint 	 l1=0;
+GLint 	 l2=0;
+GLint 	 eyeX=21;
+GLint 	 eyeY=7;
+GLint 	 eyeZ=10;
+GLint 	 centerX=-10;
+GLint 	 centerY=-10;
+GLint 	 centerZ=10;
+GLint 	 upX=0;
+GLint 	 upY=1;
+GLint 	 upZ=0;
 GLint    defineView=0;
 GLint    defineProj=1;
 GLfloat  obsP[5];
-
 
 //iluminacao
 int light=1;
@@ -78,15 +86,16 @@ GLfloat pretoDiffuse[]={0.01,0.01,0.01,1};
 GLfloat pretoSpecular[]={0.01,0.01,0.01};
 GLfloat pretoShininess=0;
 
-GLfloat GlassDiffuse []={ 0.1, 0.1, 0.1, 0.3};
+//Transparencia
+GLfloat GlassDiffuse []={ 0.1, 0.1, 0.1, 0.7};
 GLfloat GlassSpecular []={ 1, 1, 1};
 GLint GlassShininess = 1;
 
 // Texturas
 GLuint  texture[10];
+GLuint  texture_ball[3];
 GLuint  tex;
 RgbImage imag;
-
 
 void criaDefineTexturas(){
 	// Chao y=0
@@ -103,7 +112,6 @@ void criaDefineTexturas(){
 		imag.GetNumRows(), 0, GL_RGB, GL_UNSIGNED_BYTE,
 		imag.ImageData());
 
-	// Parede principal
 	glGenTextures(1, &texture[2]);
  	glBindTexture(GL_TEXTURE_2D, texture[2]);
  	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_LINEAR);
@@ -117,7 +125,6 @@ void criaDefineTexturas(){
 	imag.GetNumRows(), 0, GL_RGB, GL_UNSIGNED_BYTE,
 	imag.ImageData());
 
- 	// Parede lado esquerdo
 	glGenTextures(1, &texture[3]);
  	glBindTexture(GL_TEXTURE_2D, texture[3]);
  	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_LINEAR);
@@ -131,40 +138,38 @@ void criaDefineTexturas(){
 	imag.GetNumRows(), 0, GL_RGB, GL_UNSIGNED_BYTE,
 	imag.ImageData());
 
-	// Parede lado direito
-	glGenTextures(1, &texture[4]);
- 	glBindTexture(GL_TEXTURE_2D, texture[4]);
- 	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_LINEAR);
- 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
- 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
- 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
- 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
- 	imag.LoadBmpFile("ld.bmp");
- 	glTexImage2D(GL_TEXTURE_2D, 0, 3,
- 	imag.GetNumCols(),
-	imag.GetNumRows(), 0, GL_RGB, GL_UNSIGNED_BYTE,
-	imag.ImageData());
+	//bola
+	glGenTextures(1, &texture_ball[0]);
+  glBindTexture(GL_TEXTURE_2D, texture_ball[0]);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+  imag.LoadBmpFile("ball0.bmp");
+  glTexImage2D(GL_TEXTURE_2D, 0, 3, imag.GetNumCols(),
+                 imag.GetNumRows(), 0, GL_RGB, GL_UNSIGNED_BYTE,
+                 imag.ImageData());
+
+  glGenTextures(1, &texture_ball[1]);
+  glBindTexture(GL_TEXTURE_2D, texture_ball[1]);
+  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+  imag.LoadBmpFile("ball1.bmp");
+  glTexImage2D(GL_TEXTURE_2D, 0, 3, imag.GetNumCols(),
+                 imag.GetNumRows(), 0, GL_RGB, GL_UNSIGNED_BYTE,
+                 imag.ImageData());
 }
 
 void iluminacao(){
 	glLightfv(GL_LIGHT0, GL_POSITION, posicaoLuz);
-
-	/*glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 1.0f);
-	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.00f);
-	glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.0f);*/
-
 	glLightfv(GL_LIGHT0, GL_AMBIENT, corLuzA);
-
 	glLightfv(GL_LIGHT0, GL_SPECULAR, corLuzE);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, corLuzD);
-
-  /* glLightfv(GL_LIGHT0, GL_POSITION, posicaoLuz);
-	 glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, direcaoLuz);
-	 glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, concentracaoLuz);
-	 glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, anguloLuz);
-	 glLightfv(GL_LIGHT0, GL_SPECULAR, corLuz);
-	 glLightfv(GL_LIGHT0, GL_DIFFUSE, corLuz);
-*/}
+}
 
 
 void init(void){
@@ -221,7 +226,7 @@ void drawScene(){
 
 	// Parede lado esquerdo
 	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D,texture[4]);
+	glBindTexture(GL_TEXTURE_2D,texture[2]);
 	glPushMatrix();
 		glBegin(GL_QUADS);
 			glNormal3f(0.0, 0.0, -1.0);
@@ -266,7 +271,6 @@ void drawScene(){
 void drawScene1(){
 		glPushMatrix();
 			glBegin(GL_QUADS);
-				glColor4f(1.0,1.0,1.0,0.5);
 				glNormal3f(-1.0, 0.0, 0.0);
 				glVertex3i( xC,  0, xC);
 				glVertex3i( xC, 0, 0 );
@@ -279,17 +283,9 @@ void drawScene1(){
 void ball_movement(){
   cube1_position_z = 5+obsP[1]-obsP[2];
 	cube1_position_x = 19.75;
-	if(ball_speed_z != 0)
-	{
-		aux_ball_speed = ball_speed_z;
-	}
 	/* caso a bola bata na parede de trás ou na da frente ( a direito )*/
 	if(ball_position_x <= 0.1 || ball_position_x >= 19.9 ){
 		ball_speed_x = ball_speed_x * -1;
-		if(ball_speed_z != 0)
-		{
-			aux_ball_speed = ball_speed_z;
-		}
 		//caso bata com angulo atrás ou à frente
 
 		//printf("Bateu nas paredes frontais\n" );
@@ -301,10 +297,6 @@ void ball_movement(){
 	{
 
 		ball_speed_z = ball_speed_z * -1;
-
-		if(ball_speed_z!=0){
-		aux_ball_speed = ball_speed_z;
-	}
 			//printf("Bateu nas paredes de laterais\n" );
 	}
 
@@ -337,8 +329,6 @@ void ball_movement(){
 			 //printf("BATEU NO CUBO\n");
 		 }
 	 }
-
-
 			//actualização
 			ball_position_x += ball_speed_x;
 			ball_position_z += ball_speed_z;
@@ -364,57 +354,39 @@ void display(void){
 	// Modelo+View(camera/observador)
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(21, 7, 10,-10,-10,10, 0, 1, 0);
+	gluLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
 
 	// Objectos
 	//cubo 1
 	glPushMatrix();
-	//glColor4f(LARANJA);
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, rubyDiffuse);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, rubySpecular );
 	glMaterialf(GL_FRONT, GL_SHININESS, rubyShininess);
-	//glColor4f(VERDE);
 	cube1_position_z = 5+obsP[1]-obsP[2];
-	glTranslatef(19.75,1,cube1_position_z);
-	draw_cube();
-	glPopMatrix();
-
-	//cubo 2
-	glPushMatrix();
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, rubyDiffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, rubySpecular);
-	glMaterialf(GL_FRONT, GL_SHININESS, rubyShininess);
-	//glColor4f(AMARELO);
-	glTranslatef(19.75,1,15+obsP[3]-obsP[4]);
+	glTranslatef(19.75,0.25,cube1_position_z);
 	draw_cube();
 	glPopMatrix();
 
 	//bola
 	glPushMatrix();
-	//glColor4f(BLACK);
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture_ball[ball_type]);
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, yellowRubberDiffuse);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, yellowRubberSpecular);
 	glMaterialf(GL_FRONT, GL_SHININESS, yellowRubberShininess);
 	glTranslatef(ball_position_x,jump_coordinate,ball_position_z);
 	glutSolidSphere(0.25,20,20);
+	glDisable(GL_TEXTURE_2D);
 	glPopMatrix();
 
-	//glEnable(GL_BLEND);
-	//glEnable(GL_DEPTH_TEST);
-	//glBlendFunc(GL_SRC_ALPHA,	GL_ONE_MINUS_SRC_ALPHA);
 	drawScene();
-	//glDisable(GL_DEPTH_TEST);
-	//glDisable(GL_BLEND);
 
-	//glDepthMask(GL_FALSE);
 	glPushMatrix();
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, GlassDiffuse);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, GlassSpecular);
 	glMaterialf (GL_FRONT, GL_SHININESS, GlassShininess);
 	drawScene1();
 	glPopMatrix();
-
-	//glDepthMask(GL_TRUE);
 
 	ball_movement();
 
@@ -438,37 +410,45 @@ void keyboard(unsigned char key, int x, int y){
 		}
 	break;
 
+	case '2':
+			if(l1 == 0){
+				eyeX=eyeX-5;
+				eyeY=eyeY+13;
+				l1=1;
+			}
+			else{
+				eyeX=eyeX+5;
+				eyeY=eyeY-13;
+				l1 = 0;
+			}
+		glutPostRedisplay();
+		break;
+
+		case '3':
+				if(l2 == 0){
+					eyeX=eyeX+7;
+					eyeY=eyeY+3;
+					l2=1;
+				}
+				else{
+					eyeX=eyeX-7;
+					eyeY=eyeY-3;
+					l2 = 0;
+				}
+			glutPostRedisplay();
+			break;
+
+	case 'q':
+	case 'Q':
+		defineProj=(defineProj+1)%2;
+		glutPostRedisplay();
+		break;
+
 	case ' ':
 		if(jump==0){
 			jump = 1;
 		}
 	break;
-
- case 'p':
- case 'P':
- if(ball_speed_z!=0)
- {
- 		aux_ball_speed = ball_speed_z;
-	}
- printf("ball_speed_z: %f\n",aux_ball_speed);
- if(pause_game == 0){
-
-			if(ball_speed_z !=0 )
-			{
-				aux_ball_speed = ball_speed_z;
-				aux_ball_speed_x = ball_speed_x;
-			}
- 			ball_speed_x = 0;
-			ball_speed_z = 0;
-			pause_game = 1;
-		}
-		else
-		{
-			ball_speed_x = aux_ball_speed_x;
-			ball_speed_z = aux_ball_speed;
-			pause_game = 0;
-		}
-	 break;
 
 	// Obj 1 anda para a esquerda
 	case 'A':
@@ -488,23 +468,11 @@ void keyboard(unsigned char key, int x, int y){
 	glutPostRedisplay();
 	break;
 
-	// Obj 2 anda para a esquerda
-	case 'j':
-	case 'J':
-	if(15+obsP[3]-obsP[4] <= xC-2){
-		obsP[3] = obsP[3] + inc;
-	}
-	glutPostRedisplay();
-	break;
-
-// O bj 2 anda para a direita
-	case 'L':
-	case 'l':
-	if(15+obsP[3]-obsP[4] >= 2){
-		obsP[4] = obsP[4] + inc;
-	}
-	glutPostRedisplay();
-	break;
+	case 'x':
+	case 'X':
+		ball_type+=1;
+		glutPostRedisplay();
+	  break;
 
 	case 27:
 		exit(0);
@@ -518,8 +486,8 @@ int main(int argc, char** argv){
 	srand(time(NULL));
 	int r = rand()%4000 - 2000; //this produces numbers between -2000 - +2000
   float random_num = r/10000.0;
-	printf("ball_speed_z : %f\n",random_num);
-	ball_speed_z =random_num;
+	//printf("ball_speed_z : %f\n",random_num);
+	ball_speed_z = random_num;
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
